2a996b596ec201333c87ff08ccd4da441781476b
Yina Zhu <yinagit@gmail.com>
2018-04-19 16:55:37 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,20 +3,25 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
 
   def initialize(attributes)
    attributes.each {|key, value| self.send(("#{key}="), value)}
  end
 
  def self.create_table
-   
+
   def self.create(name:, grade:)
-    student = Student.new(name, grade)
-    student.save
-    
-    student
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT
+      grade TEXT
+    )
+    SQL
+    DB[:conn].execute(sql)
+
   end
  end
-  
+
 end
