01708932527ddab9bfdb2d10abb0221e0cdc2903
Yina Zhu <yinagit@gmail.com>
2018-04-19 17:07:42 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,7 +4,7 @@ class Student
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
-  
+
   def initialize(name, grade, id=nil)
       @id = id
       @name = name
