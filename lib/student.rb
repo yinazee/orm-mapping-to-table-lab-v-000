22238439479454b1ec2cdd94b69fd80c1e932e87
Yina Zhu <yinagit@gmail.com>
2018-04-19 21:23:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,27 +29,27 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  # def save
-  #   sql = <<-SQL
-  #     INSERT INTO students (name, grade)
-  #     VALUE (?, ?)
-  #     SQL
-  #
-  #     DB[:conn].execute(sql, self.name, self.grade)
-  #     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-  #     #  grab the ID of the last inserted row, i.e. the row you just inserted into the database,
-  #     # and assign it to the be the value of the @id attribute of the given instance
-  # end
-
   def save
-  sql = <<-SQL
-    INSERT INTO students (name, grade)
-    VALUES (?, ?)
-  SQL
+    sql = <<-SQL
+      INSERT INTO students (name, grade)
+      VALUE (?, ?)
+      SQL
+  
+      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+      #  grab the ID of the last inserted row, i.e. the row you just inserted into the database,
+      # and assign it to the be the value of the @id attribute of the given instance
+  end
 
-  DB[:conn].execute(sql, self.name, self.grade)
-  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-end
+#   def save
+#   sql = <<-SQL
+#     INSERT INTO students (name, grade)
+#     VALUES (?, ?)
+#   SQL
+# 
+#   DB[:conn].execute(sql, self.name, self.grade)
+#   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
+# end
 
 
 end
