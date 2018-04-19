addb350e932720c0a221d128f417f09306531f16
Yina Zhu <yinagit@gmail.com>
2018-04-19 21:27:16 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,9 +42,9 @@ class Student
   end
 
   def self.creates(name:, grade:)
-    student = self.new(name, grade)
-    self.save
-    self
+    student = Student.new(name, grade)
+    student.save
+    student
   end
 
 
