cf9372ded6b02d12d7fc953d7b721492d4100cb7
Yina Zhu <yinagit@gmail.com>
2018-04-19 19:20:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -35,7 +35,7 @@ class Student
       VALUE (?, ?)
       SQL
 
-      DB[]
+      DB[:conn].execute(sql, self.name, self.grade)
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
       #  grab the ID of the last inserted row, i.e. the row you just inserted into the database,
       # and assign it to the be the value of the @id attribute of the given instance
