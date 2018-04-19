28b04d1c88eb891af0330265db312e671875e790
Yina Zhu <yinagit@gmail.com>
2018-04-19 17:56:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,14 +31,12 @@ class Student
 
   def save
     sql = <<-SQL
-      INSERT INTO students(name, grade)
-      VALUE (?, ?)
-      SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
 
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
-      #  grab the ID of the last inserted row, i.e. the row you just inserted into the database,
-      # and assign it to the be the value of the @id attribute of the given instance
-    binding.pry
+    DB[:conn].execute(sql, self.name, self.grade)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
 
